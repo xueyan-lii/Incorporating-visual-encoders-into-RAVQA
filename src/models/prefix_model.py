f5d93ed616be108a95365c0e83b58b28915d79ea
Xueyan Li <lixueyan1998@outlook.com>
2023-06-10 14:54:28 +0100

    Create NoDPR model to use only prefix and question

---
+++ src/models/prefix_model.py
@@ -0,0 +1,178 @@
+import copy
+import math
+import os
+from turtle import forward
+import warnings
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.nn import CrossEntropyLoss
+from torch.utils.checkpoint import checkpoint
+from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
+from collections import Counter, defaultdict
+from easydict import EasyDict
+from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config, T5PreTrainedModel
+import pytorch_lightning as pl
+#from datasets import load_from_disk
+import time
+
+class MLP(nn.Module):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
+
+    def __init__(self, sizes: Tuple[int, ...], bias=True, act=nn.Tanh):
+        super(MLP, self).__init__()
+        layers = []
+        for i in range(len(sizes) - 1):
+            layers.append(nn.Linear(sizes[i], sizes[i + 1], bias=bias))
+            if i < len(sizes) - 2:
+                layers.append(act())
+        self.model = nn.Sequential(*layers)
+
+class PrefixModel(pl.LightningModule):
+    '''
+    Class to only add image prefix embedding to question without any text captioning or document
+    '''
+    def __init__(self, config: EasyDict, data_loader) -> None:
+        super().__init__()
+
+        self.config = config
+        self.data_loader = data_loader
+        self.tokenizer = data_loader.tokenizer
+
+        # Initialising generator
+        GeneratorModelClass = globals()[self.config.model_config.GeneratorModelClass]
+        GeneratorConfigClass = globals()[self.config.model_config.ConfigClass]
+        generator_model_config = GeneratorConfigClass.from_pretrained(self.config.model_config.ModelVersion)
+        self.generator = GeneratorModelClass.from_pretrained(self.config.model_config.ModelVersion,
+                                                    config=generator_model_config)
+        self.generator.resize_token_embeddings(len(self.tokenizer))
+
+        self.lm_embedding_size = self.generator.model_dim 
+        self.prefix_length = 32
+        self.prefix_size = 768  
+            
+        if self.config.model_config.UsePrefixEmb == 2:
+            print("\n Using MLP")
+            print("MLP input ",self.prefix_size, ' Hidden dim ',(self.lm_embedding_size * self.prefix_length) // 2, ' Output dim ',self.lm_embedding_size,' x ', self.prefix_length)
+            self.clip_project = MLP(
+                (
+                    self.prefix_size,
+                    (self.lm_embedding_size * self.prefix_length) // 2,
+                    self.lm_embedding_size * self.prefix_length,
+                )
+            )
+        elif self.config.model_config.UsePrefixEmb == 1:
+            print("\n Using MLP")
+            print("MLP input ",self.prefix_size,' Output dim ',self.lm_embedding_size,' x ', self.prefix_length)
+            self.clip_project = MLP(
+                (
+                    self.prefix_size,
+                    self.lm_embedding_size * self.prefix_length,
+                )
+            )
+
+        #for qformer op, prefix_length has to be 32 cannot be changed
+        elif self.config.model_config.UsePrefixEmb == 0.5: 
+            print("\n Using MLP for Qformer")
+            print("MLP input ",self.prefix_size,' Output dim ',self.lm_embedding_size)
+            self.clip_project = MLP(
+                (
+                    self.prefix_size,
+                    self.lm_embedding_size,
+                )
+            )
+
+        else:
+            print('\n No MLP or image prefix used')
+
+    def insert_prefix_into_emb(self, batch_size, batch_text_tokens, batch_text_masks, batch_prefix_projections, labels):
+        no_documents=1
+        prefix_len = batch_prefix_projections.shape[1]
+        text_len = batch_text_tokens.shape[1]
+        emb_dim = batch_prefix_projections.shape[2]
+        batch_text_embeddings = self.generator.shared(batch_text_tokens)
+
+        embedding_out=torch.ones((no_documents*batch_size, prefix_len+text_len, emb_dim), dtype=int, device=labels.device)*-100.0 
+        mask_out=torch.ones((no_documents*batch_size, prefix_len+text_len),dtype=int, device=labels.device)*-100
+        batch_inds_for_broadcasting=[[i] for i in range(no_documents*batch_size)]
+        text_tokens_mask=torch.zeros((no_documents*batch_size, prefix_len+text_len),dtype=int, device=labels.device)
+        text_tokens_mask[batch_inds_for_broadcasting, torch.arange(text_len)+prefix_len] =1
+        embedding_out[text_tokens_mask.bool()]=batch_text_embeddings.view(-1,emb_dim)
+        for i in range(batch_size):
+            prefix_mask = torch.zeros((no_documents*batch_size, prefix_len+text_len),dtype=int, device=labels.device).bool()
+            prefix_mask[[[j] for j in range(i*no_documents, (i+1)*no_documents)], torch.arange(prefix_len, device=labels.device)]=1
+            embedding_out[prefix_mask] = batch_prefix_projections[i].repeat(no_documents,1,1).view(-1,emb_dim)
+
+        mask_out[~text_tokens_mask.bool()]=1
+        mask_out[text_tokens_mask.bool()] = batch_text_masks.view(-1)
+        return embedding_out, mask_out
+        
+
+    def forward(self, input_ids: torch.Tensor,
+                      attention_mask: torch.Tensor,
+                      labels: torch.Tensor,
+                      prefix: torch.Tensor,
+                    **kwargs):
+        
+        batch_size = input_ids.shape[0]
+
+        if self.config.model_config.UsePrefixEmb:
+            prefix_projections = self.clip_project(prefix).view(-1, self.prefix_length, self.lm_embedding_size)
+            joint_embeddings, joint_attention_masks = self.insert_prefix_into_emb(batch_size=batch_size, batch_text_tokens=input_ids, 
+                                            batch_text_masks=attention_mask, 
+                                            batch_prefix_projections=prefix_projections,
+                                            labels=labels)
+            
+            generator_outputs = self.generator(
+                            inputs_embeds=joint_embeddings,
+                            attention_mask=joint_attention_masks,
+                            labels=labels)
+        
+        else:
+            generator_outputs = self.generator(
+                            input_ids=input_ids,
+                            attention_mask=attention_mask,
+                            labels=labels)
+        
+        return EasyDict(loss=generator_outputs.loss)
+
+
+    def generate(self, input_ids: torch.Tensor,
+                      attention_mask: torch.Tensor,
+                      prefix: torch.Tensor,
+                      **kwargs):
+
+        batch_size = input_ids.shape[0]
+        
+        if self.config.model_config.UsePrefixEmb:
+            prefix_projections = self.clip_project(prefix).view(-1, self.prefix_length, self.lm_embedding_size)
+            joint_embeddings, joint_attention_masks = self.insert_prefix_into_emb(batch_size=batch_size, batch_text_tokens=input_ids, 
+                                            batch_text_masks=attention_mask, 
+                                            batch_prefix_projections=prefix_projections,
+                                            labels=input_ids)
+            
+            test_batch = EasyDict({
+            'inputs_embeds': joint_embeddings,
+            'attention_mask': joint_attention_masks,
+            })
+        
+        else:
+            test_batch = EasyDict({
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+        })
+        
+        # Get encoder outputs first
+        encoder_outputs = self.generator.encoder(
+            **test_batch
+        )
+
+        # Get decoder outputs from encoder_outputs
+        test_batch = {
+            'encoder_outputs': encoder_outputs,
+            "max_length": self.config.data_loader.additional.max_target_length,
+        }
+        generation_outputs = self.generator.generate(**test_batch)
+        return generation_outputs
---
+++ src/scripts/NoDPR.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH -J RAVQA_FrDPR_train_cc
+#SBATCH -A MLMI-xl544-SL2-GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=15:00:00
+#SBATCH --mail-type=FAIL
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+#SBATCH --no-requeue
+#SBATCH -p ampere
+#! ############################################################
+
+LOG=/dev/stdout
+ERR=/dev/stderr
+EXP_NAME=OKVQA_NoDPR-flanT5-noprefix
+# UNCOMMENT BELOW FOR SLURM SBATCH
+. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
+module purge                               # Removes all modules still loaded
+module load rhel8/default-amp
+module load cuda/11.1 intel/mkl/2017.4
+#source scripts/activate_my_env.sh
+JOBID=$SLURM_JOB_ID
+LOG=../logs/$EXP_NAME-log.$JOBID
+ERR=../logs/$EXP_NAME-err.$JOBID
+
+python main.py ../configs/okvqa/T5_NoDPR_prefix_only.jsonnet \
+    --mode train \
+    --experiment_name ${EXP_NAME}.$JOBID  \
+    --accelerator auto --devices 1  \
+    --num_sanity_val_steps 2 \
+    --opts train.epochs=10  \
+            train.batch_size=1  \
+            valid.step_size=0.25  \
+            valid.batch_size=32  \
+            train.additional.gradient_accumulation_steps=32  \
+            train.lr=0.00006  \
+            train.MLP_lr=0.0005 \
+            train.scheduler=linear \
+            model_config.UsePrefixEmb=0 \
+            model_config.UseQformerEmb=1 \
+            model_config.TokenizerModelVersion="google/flan-t5-large" \
+            model_config.ModelVersion="google/flan-t5-large" \
+   >> $LOG 2> $ERR
+
+# once UsePrefixEmb is set to 0, no prefix is used (only question exist, MLP not trained), setting UseQformerEmb will not do anything
+# testing only from checkpoint
+# python main.py \
+#    ../configs/okvqa/RAVQA.jsonnet  \
+#    --mode test  \
+#    --experiment_name ${EXP_NAME}.$JOBID \
+#    --accelerator auto --devices 1  \
+#    --modules freeze_question_encoder force_existence  \
+#    --log_prediction_tables   \
+#    --opts data_loader.additional.num_knowledge_passages=5 \
+#            test.load_model_path=../Experiments/OKVQA_RA-VQA-FrDPR_FullCorpus.19792250/train/saved_model/model_6.ckpt \
+#   >> $LOG 2> $ERR
